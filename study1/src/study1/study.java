c3e4979b775701bb7992bb04deb9620639ca4691
junhyungkwon <aofry@naver.com>
2021-04-16 20:31:39 +0900

    반복문에서 복합 대입 연산자 공부

---
+++ study1/src/study1/study.java
@@ -0,0 +1,23 @@
+package study1;
+
+public class study {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 1;
+		num += 2;
+		num += 3;
+		num += 4;
+		num += 5;
+		num += 6;
+		num += 7;
+		num += 8;
+		num += 9;
+		num += 10;
+		
+		System.out.println("1부터 10까지의 합은" + num + "입니다");
+		
+		
+	}
+
+}
